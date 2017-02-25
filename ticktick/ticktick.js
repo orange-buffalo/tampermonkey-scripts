395a8e048c366694f70f60d4f201fde11b4ca9f6
orange-buffalo <orange-buffalo@users.noreply.github.com>
2017-02-25 10:48:54 +0100

    verifying autoupdate

--- ticktick/ticktick.js
+++ ticktick/ticktick.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ticktick
-// @namespace    http://tampermonkey.net/
-// @version      0.1
+// @namespace    https://github.com/orange-buffalo/tampermonkey-scripts/tree/master/ticktick
+// @version      0.2
 // @updateURL    https://raw.githubusercontent.com/orange-buffalo/tampermonkey-scripts/master/ticktick/ticktick.js
 // @downloadURL  https://raw.githubusercontent.com/orange-buffalo/tampermonkey-scripts/master/ticktick/ticktick.js
 // @author       orange-buffalo
